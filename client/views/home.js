e4a4ee740c28ca2572175b283a76c95a6d70f876
Ononos <anonym107@gmail.com>
2015-04-10 20:06:17 +0300

    Add .less and, tooltip date from now, url navs: data, dj

--- client/views/home.js
+++ client/views/home.js
@@ -5,17 +5,33 @@ Template.fileRow.helpers({
     } else
       return '';
   },
-  fromNow: function() {
-    if (typeof this.t !== 'undefined') {
-      return moment(this.t).fromNow();
-    } else {
-      return "Неизвестно";
-    }
-  },
   scheduledClass: function() {
     var prevSchedule = Records.findOne({t: {$lte: this.t}, isSch: true});
     return (prevSchedule &&
             prevSchedule.t.getTime() + prevSchedule.duration * 1000 >
             this.t.getTime()) ? "scheduled" : "";
+  },
+  durationHHMM: function() {
+    var duration = this.duration;
+    if (_.isUndefined(duration))
+      return "??:??";
+
+    var h = parseInt(duration / 3600),
+        m = parseInt((duration - h * 3600) / 60);
+    return  ((h<10) ? '0' + h : h) + ':' + ((m<10) ? '0' + m : m);
+  }
+});
+
+Template.fileRow.rendered = function () {
+  this.$('[data-action="setDate"]')
+    .tooltip({
+      placement: "right",
+      title: moment(this.data.t).fromNow()
+    });
+};
+
+Template.fileRow.events({
+  'click [data-action="setDate"]': function(e, t) {
+    Session.set("filter-date", this.t);
   }
 });
