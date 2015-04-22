8ce0c35be7619785af4c5f089e7ee139f75ade11
Ononos <anonym107@gmail.com>
2015-04-22 16:32:55 +0300

    fix paginator

--- client/views/help_paginator.js
+++ client/views/help_paginator.js
@@ -2,32 +2,47 @@
 /*
  Usage:
  
- {{>paginator route="myLiked" page=1 pages=9 perPage=2}}
+   {{>paginator route="myLiked" page=1 pages=9}}
 
  url to page: route must have param "page"!
+
+   Router.route('/liked/:page', {
 */
 
 var pagination_neighborhood = 4;
 
 Template.paginator.helpers({
   farFromStart: function() {
-    return Math.max (0, this.page - pagination_neighborhood);
+    return Math.max (0, +this.page - pagination_neighborhood);
   },
 
   farToEnd: function() {
-    console.log('farToEnd', this.pages - pagination_neighborhood , this.page);
-    return this.pages - pagination_neighborhood -1 > this.page;
+    return this.pages - pagination_neighborhood > +this.page;
   },
 
   neighborhood: function() {
-    var from = Math.max(0, this.page - pagination_neighborhood),
-        to = Math.min(this.pages, from + this.perPage * pagination_neighborhood + 1);
+    var from = Math.max(0, +this.page - pagination_neighborhood),
+        to = Math.min(+this.pages, +this.page + pagination_neighborhood),
+        need_delta = 2 * pagination_neighborhood;
 
-    return _.range(from, to);
+    if (to - from < need_delta) {
+      if (+this.page < pagination_neighborhood) {
+        to += need_delta - to;
+        to = Math.min(+this.pages, to);
+      }
+      if (+this.page > this.pages - pagination_neighborhood) {
+        from -= pagination_neighborhood - (this.pages - this.page);
+        from = Math.max(0, from);
+      }
+    }
+      
+
+    console.log('from', from, 'to', to, '++ delta', need_delta);
+    return _.range(from, to + 1);
   },
   isThisPage: function() {
     return this == Template.parentData(1).page;
   },
 
-  lastPage: function() { return this.pages - 1;}
+  lastPage: function() { return this.pages;}
 });
